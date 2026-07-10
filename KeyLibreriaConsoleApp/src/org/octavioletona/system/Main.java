4bc16b6c72e1c73c4b036a3d6ef85a5a2b7ae9ad
Octavio Letona <oletona-2026097@kinal.edu.gt>
2026-07-10 08:25:13 -0600

    001 - se agreo clases Conexion y MenuPrincipal, se editó clase Main

--- KeyLibreriaConsoleApp/src/org/octavioletona/system/Main.java
+++ KeyLibreriaConsoleApp/src/org/octavioletona/system/Main.java
@@ -4,17 +4,21 @@
  */
 package org.octavioletona.system;
 
+import org.key.view.MenuPrincipal;
+
 /**
  *
- * @author informatica
+import org.key.view.MenuPrincipal;
+
+/**
+ *
+ * @author 
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        MenuPrincipal menu = new MenuPrincipal();
+        menu.iniciar();
     }
     
 }
