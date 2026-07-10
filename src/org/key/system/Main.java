54285f29a33275d2988238bb3f1bf35126ce94bf
Informatica <informatica@alumno.local>
2026-07-10 10:00:27 -0600

    primer commit Autor 1

---
+++ src/org/key/system/Main.java
@@ -0,0 +1,16 @@
+package org.key.system;
+
+import org.key.view.MenuPrincipal;
+
+/**
+ *
+ * @author aruba
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        MenuPrincipal menu = new MenuPrincipal();
+        menu.iniciar();
+    }
+    
+}
