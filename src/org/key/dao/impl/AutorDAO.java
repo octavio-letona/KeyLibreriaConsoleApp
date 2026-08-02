dda98d3612e212bb4fea2f3d2973f4a74ca02eab
Octavio Letona <octaviojletona@gmail.com>
2026-08-01 20:06:08 -0600

    feat: agregamos vista controllador y todo el molde de nuestro autor

--- src/org/key/dao/impl/AutorDAO.java
+++ src/org/key/dao/impl/AutorDAO.java
@@ -1,13 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 package org.key.dao.impl;
 
 /**
  *
- * @author informatica
+ * @author sheyl
  */
-public class gitkeep {
+public interface AutorDAO {
     
 }
