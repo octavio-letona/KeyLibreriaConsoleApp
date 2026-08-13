e7ee837833d26167fdb89a45f00b6fca1a13cbf1
cristofer Emmanuel Gutierrez Barrientos <cgutierrez-2026120@kinal.edu.gt>
2026-08-13 09:18:22 -0600

    fix: arreglamos errores en la vista

--- src/org/key/controller/MenuPrincipalController.java
+++ src/org/key/controller/MenuPrincipalController.java
@@ -1,17 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
 package org.key.controller;
-
+ 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import org.key.system.Main;
-
+ 
 //falta el extends initilize
 public class MenuPrincipalController {
-
+ 
     @FXML
     private void handleClientes() {
         try {
@@ -19,40 +15,37 @@ public class MenuPrincipalController {
         } catch (Exception e) {
             mostrarError("Error al cargar la vista de clientes:\n" + e.getMessage());
         }
-        
     }
-
+ 
     @FXML
     private void handleEditorial() {
         try {
             // Verifica que este nombre de archivo coincida con mayúsculas y minúsculas exactas
             Main.cambiarVista("/org/key/view/EditorialView.fxml");
         } catch (Exception e) {
-            mostrarError("Error al cargar la vista de editoriales:\n" + e.toString());
+            mostrarError("Error al cargar la vista de editoria:\n" + e.toString());
         }
     }
-    
     @FXML
     private void handleAutores() {
         try {
             // Verifica que este nombre de archivo coincida con mayúsculas y minúsculas exactas
-            Main.cambiarVista("/org/key/view/EditorialView.fxml");
+            Main.cambiarVista("/org/key/view/autorview.fxml");
         } catch (Exception e) {
-            mostrarError("Error al cargar la vista de editoriales:\n" + e.toString());
+            mostrarError("Error al cargar la vista de Aurotes:\n" + e.toString());
         }
     }
-    
-    
+
         @FXML
     private void handleCategorias() {
         try {
             // Verifica que este nombre de archivo coincida con mayúsculas y minúsculas exactas
-            Main.cambiarVista("/org/key/view/EditorialView.fxml");
+            Main.cambiarVista("/org/key/view/CategoriaView.fxml");
         } catch (Exception e) {
-            mostrarError("Error al cargar la vista de editoriales:\n" + e.toString());
+            mostrarError("Error al cargar la vista de Categorias:\n" + e.toString());
         }
     }
-
+ 
     @FXML
     private void handleNoDisponible() {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
@@ -61,12 +54,12 @@ public class MenuPrincipalController {
         alert.setContentText("Este módulo no está disponible aún.");
         alert.showAndWait();
     }
-
+ 
     @FXML
     private void handleSalir() {
         Platform.exit();
     }
-
+ 
     private void mostrarError(String mensaje) {
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle("Error");
@@ -74,5 +67,5 @@ public class MenuPrincipalController {
         alert.setContentText(mensaje);
         alert.showAndWait();
     }
-
+ 
 }
