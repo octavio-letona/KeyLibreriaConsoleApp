a915b17b5805dbb64dcd60d5f5135560c79d8f48
octavio letona <oletona-2026097@kinal.edu.gt>
2026-08-14 11:12:15 -0600

    feat: subimos la rama de editorial

--- src/org/key/controller/AdminDashboradController.java
+++ src/org/key/controller/AdminDashboradController.java
@@ -2,6 +2,7 @@ package org.key.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
@@ -9,18 +10,20 @@ import org.key.model.Usuario;
 
 public class AdminDashboradController implements Initializable {
 
-    @FXML private Label lblBienvenida;
+    @FXML
+    private Label lblBienvenida;
     private Usuario usuarioActual;
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }
 
-    public void iniciarUsuario(Usuario usuario){
+    public void iniciarUsuario(Usuario usuario) {
         this.usuarioActual = usuario;
         if (lblBienvenida != null && usuario != null) {
             lblBienvenida.setText("Bienvenido administrador " + usuario.getUsername());
         }
     }
+
 }
